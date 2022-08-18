8b0b2e5563eda8f294a07711394880b100a117a2
newkayak12 <newkayak12@gmail.com>
2022-08-18 20:48:28 +0900

    INIT swift-homework
    1. accumulator

---
+++ Accumulator/Accumulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accumulator
+//
+//  Created by Sang Hyeon kim on 2022/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
