9e8b5a1b2d977d97893f3b7904ccf2b6fb50cfd5
newkayak12 <newkayak12@gmail.com>
2022-08-18 22:36:14 +0900

    swift_homework

--- Accumulator/Accumulator/ViewController.swift
+++ Accumulator/Accumulator/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    
+    var numRef: Int = 0;
+    @IBOutlet weak var number: UILabel!
+    @IBAction func plus(_ sender: Any) {
+        numRef += 1;
+        number.text = "\(numRef)"
+    }
+    @IBAction func minus(_ sender: Any) {
+        numRef -= 1;
+        number.text = "\(numRef)"
+    }
 }
 
